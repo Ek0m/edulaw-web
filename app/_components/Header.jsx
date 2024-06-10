4a90aed9ab09e04974ee9d2ba18b3fcdfffee7af
Ek0m <91160565+Ek0m@users.noreply.github.com>
2024-06-10 20:17:53 +0100

    header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,12 +5,12 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="p-6 flex items-center justify-between">
+    <div className="p-6 flex items-center justify-center lg:justify-between">
       <div className="flex items-center ">
         <Image src="/logo.png" alt="img" width={110} height={100} />
         <h1 className="text-4xl">EduLaw</h1>
       </div>
-      <Link href={"/questions"}>
+      <Link href={"/questions"} className="hidden lg:flex">
         <Button className=" p-5 lg:p-6 text-md lg:text-lg">Get Started</Button>
       </Link>
     </div>
